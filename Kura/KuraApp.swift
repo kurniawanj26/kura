96a75b82ecb0acc08659d3535e72297aa0a789f8
“kurniawanj26” <jayadi.kurniawan@gmail.com>
2023-02-19 02:50:04 +0700

    Initial Commit

---
+++ Kura/KuraApp.swift
@@ -0,0 +1,17 @@
+//
+//  KuraApp.swift
+//  Kura
+//
+//  Created by Jayadi Kurniawan on 19/02/23.
+//
+
+import SwiftUI
+
+@main
+struct KuraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
