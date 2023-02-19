be8e1cfd45ba1a812cfbf5432b67c5cdd2885690
“kurniawanj26” <jayadi.kurniawan@gmail.com>
2023-02-20 00:07:23 +0700

    bug fixed live preview not working (set the icon to Universal instead of iPhone) 👏🏻 set up the tab bar created extension helper for setting up tab bar icon color 🚀

---
+++ Kura/Helpers/Extensions.swift
@@ -0,0 +1,28 @@
+//
+//  Extensions.swift
+//  Kura
+//
+//  Created by Jayadi Kurniawan on 19/02/23.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color{
+    
+    struct MyTheme {
+        
+        static var blueColor: Color {
+            return Color("ColorBlue")
+        }
+        
+        static var lighBlueColor: Color {
+            return Color("ColorLightBlue")
+        }
+        
+        static var beigeColor: Color {
+            return Color("ColorBeige")
+        }
+    }
+    
+}
---
+++ Kura/Views/Screens/ContentView.swift
@@ -0,0 +1,44 @@
+//
+//  ContentView.swift
+//  Kura
+//
+//  Created by Jayadi Kurniawan on 19/02/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        TabView {
+            Text("Screen 1")
+                .tabItem {
+                    Image(systemName: "book.fill")
+                    Text("Feed")
+                }
+            Text("Screen 2")
+                .tabItem {
+                    Image(systemName: "magnifyingglass")
+                    Text("Browse")
+                }
+            Text("Screen 3")
+                .tabItem {
+                    Image(systemName: "square.and.arrow.up.fill")
+                    Text("Upload")
+                }
+            Text("Screen 4")
+                .tabItem {
+                    Image(systemName: "person.fill")
+                    Text("Profile")
+                }
+        }
+        // set the icon and text color using colorset in Assets
+        // use the extensions from Helpers
+        .accentColor(Color.MyTheme.blueColor)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
