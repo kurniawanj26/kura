92c3d0d8901ef9d03598d6282061efa77bd3be5c
“kurniawanj26” <jayadi.kurniawan@gmail.com>
2023-02-20 00:53:15 +0700

    created feed view (scrollview to render items) created post view (item list) setting up data structure for posts

---
+++ Kura/Views/Screens/FeedView.swift
@@ -0,0 +1,31 @@
+//
+//  FeedView.swift
+//  Kura
+//
+//  Created by Jayadi Kurniawan on 20/02/23.
+//
+
+import SwiftUI
+
+struct FeedView: View {
+    
+    @ObservedObject var posts:PostArrayObject
+    
+    var body: some View {
+        ScrollView(.vertical, showsIndicators: false, content: {
+            ForEach(posts.dataArray, id: \.self) { post in
+                PostView(post: post)
+            }
+        })
+        .navigationTitle("Feed")
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+struct FeedView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+            FeedView(posts: PostArrayObject())
+        }
+    }
+}
---
+++ Kura/Views/Subviews/PostView.swift
@@ -0,0 +1,83 @@
+//
+//  PostView.swift
+//  Kura
+//
+//  Created by Jayadi Kurniawan on 20/02/23.
+//
+
+import SwiftUI
+
+struct PostView: View {
+    
+    @State var post: PostModel
+    
+    var body: some View {
+        VStack(alignment: .center, spacing: 0, content: {
+            
+            // MARK: HEADER
+            HStack {
+                Image("dog1")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 30, height: 30, alignment: .center)
+                    .cornerRadius(15)
+                
+                Text(post.username)
+                    .font(.callout)
+                    .fontWeight(.medium)
+                    .foregroundColor(.primary)
+                
+                Spacer()
+                
+                Image(systemName: "ellipsis")
+                    .font(.headline)
+            }
+            .padding(.all,6)
+            
+            // MARK: IMAGE
+            Image("dog1")
+                .resizable()
+                .scaledToFit()
+            
+            // MARK: FOOTER
+            HStack(alignment: .center, spacing: 20, content: {
+                
+                Image(systemName: "heart")
+                    .font(.title3)
+                
+                Image(systemName: "bubble.middle.bottom")
+                    .font(.title3)
+                
+                Image(systemName: "paperplane")
+                    .font(.title3)
+                
+                Spacer()
+            })
+            .padding(.all,6)
+            
+            // need to check first
+            // because in the PostModel
+            // caption is optional
+            if let caption = post.caption {
+                HStack {
+                    Text(caption)
+                    
+                    // if the caption as big as the screen width, set the spacer to 0
+                    // else it will default to certain amount
+                    Spacer(minLength: 0)
+                }
+                .padding(.all,6)
+            }
+        })
+    }
+}
+
+struct PostView_Previews: PreviewProvider {
+    
+    static var post: PostModel = PostModel(postID: "", userID: "", username: "Kuy", caption: "My first post!", dateCreated: Date(), likeCount: 0, likedByOwner: false)
+    
+    static var previews: some View {
+        PostView(post: post)
+            .previewLayout(.sizeThatFits)
+    }
+}
