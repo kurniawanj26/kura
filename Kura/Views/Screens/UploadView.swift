e851fd35b378406adacf1340c576afb6a0047640
“kurniawanj26” <jayadi.kurniawan@gmail.com>
2023-07-29 00:18:59 +0700

    added logic for take photo and import photo

--- Kura/Views/Screens/UploadView.swift
+++ Kura/Views/Screens/UploadView.swift
@@ -6,15 +6,24 @@
 //
 
 import SwiftUI
+import UIKit
 
 struct UploadView: View {
+    
+    @State var showImagePicker: Bool = false
+    @State var imageSelected: UIImage = UIImage(named: "logo")!
+    @State var sourceType: UIImagePickerController.SourceType = .camera
+    
+    
     var body: some View {
         // ZStack allow us to stack the items on top of each other at the same spot
         ZStack {
             VStack(spacing: 0) {
                 
+                // BUTTON TAKE PHOTO
                 Button(action: {
-                    
+                    sourceType = UIImagePickerController.SourceType.camera
+                    showImagePicker.toggle()
                 }, label: {
                     Text("Take photo".uppercased())
                         .font(.largeTitle)
@@ -24,8 +33,10 @@ struct UploadView: View {
                 .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
                 .background(Color.MyTheme.purpleColor)
                 
+                // BUTTON IMPORT PHOTO
                 Button(action: {
-                    
+                    sourceType = UIImagePickerController.SourceType.photoLibrary
+                    showImagePicker.toggle()
                 }, label: {
                     Text("Import photo".uppercased())
                         .font(.largeTitle)
@@ -35,6 +46,9 @@ struct UploadView: View {
                 .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
                 .background(Color.MyTheme.yellowColor)
             }
+            .sheet(isPresented: $showImagePicker, content: {
+                ImagePicker(imageSelected: $imageSelected, sourceType: $sourceType)
+            })
             
             Image("logo.transparent")
                 .resizable()
