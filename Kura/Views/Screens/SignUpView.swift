c9ad0169fb35981f39cd184cabc39920206471c9
“kurniawanj26” <jayadi.kurniawan@gmail.com>
2023-08-08 09:09:55 +0700

    created a screen to funnel new users to sign up

---
+++ Kura/Views/Screens/SignUpView.swift
@@ -0,0 +1,60 @@
+//
+//  SignUpView.swift
+//  Kura
+//
+//  Created by Jayadi Kurniawan on 08/08/23.
+//
+
+import SwiftUI
+
+struct SignUpView: View {
+    var body: some View {
+        VStack(alignment: .center, spacing: 20, content: {
+            
+            Spacer()
+            
+            Image("logo.transparent")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 100, height: 100, alignment: .center)
+            
+            Text("You're not signed in!")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .lineLimit(1)
+                .minimumScaleFactor(0.5)
+                .foregroundColor(Color.MyTheme.purpleColor)
+            
+            Text("Sign up now and join the fun community 🥳")
+                .font(.headline)
+                .fontWeight(.medium)
+                .multilineTextAlignment(.center)
+            
+            Button(action: {
+                
+            }, label: {
+                Text("Sign in / Sign Up".uppercased())
+                    .font(.headline)
+                    .fontWeight(.bold)
+                    .padding()
+                    .frame(maxWidth: .infinity)
+                    .frame(height: 60)
+                    .background(Color.MyTheme.purpleColor)
+                    .cornerRadius(12)
+                    .shadow(radius: 12)
+            })
+            .accentColor(Color.MyTheme.yellowColor)
+            
+            Spacer()
+            
+        })
+        .padding(.all, 40)
+        .edgesIgnoringSafeArea(.all)
+    }
+}
+
+struct SignUpView_Previews: PreviewProvider {
+    static var previews: some View {
+        SignUpView()
+    }
+}
