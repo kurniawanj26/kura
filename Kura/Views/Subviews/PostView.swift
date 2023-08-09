f994d10e23b5e45403e92cc26f445e1271185a42
“kurniawanj26” <jayadi.kurniawan@gmail.com>
2023-08-09 09:40:19 +0700

    configured logics to show action sheet for reporting a post

--- Kura/Views/Subviews/PostView.swift
+++ Kura/Views/Subviews/PostView.swift
@@ -11,9 +11,16 @@ struct PostView: View {
     
     @State var post: PostModel
     var showHeaderAndFooter: Bool
-
+    
     @State var animateLike: Bool = false
     @State var addHeartAnimationToView: Bool
+    @State var showActionSheet: Bool = false
+    @State var actionSheetType: PostActionSheetOption = .general
+    
+    enum PostActionSheetOption {
+        case general
+        case reporting
+    }
     
     var body: some View {
         VStack(alignment: .center, spacing: 0, content: {
@@ -25,22 +32,31 @@ struct PostView: View {
                     NavigationLink(
                         destination: ProfileView(isMyProfile: false, profileUserID: post.userID, profileDisplayName: post.username),
                         label: {
-                        Image("dog1")
-                            .resizable()
-                            .scaledToFill()
-                            .frame(width: 30, height: 30, alignment: .center)
-                            .cornerRadius(15)
-                        
-                        Text(post.username)
-                            .font(.callout)
-                            .fontWeight(.medium)
-                            .foregroundColor(.primary)
-                    })
+                            Image("dog1")
+                                .resizable()
+                                .scaledToFill()
+                                .frame(width: 30, height: 30, alignment: .center)
+                                .cornerRadius(15)
+                            
+                            Text(post.username)
+                                .font(.callout)
+                                .fontWeight(.medium)
+                                .foregroundColor(.primary)
+                        })
                     
                     Spacer()
                     
-                    Image(systemName: "ellipsis")
-                        .font(.headline)
+                    Button(action: {
+                        showActionSheet.toggle()
+                    }, label: {
+                        Image(systemName: "ellipsis")
+                            .font(.headline)
+                    })
+                    .accentColor(.primary)
+                    .actionSheet(isPresented: $showActionSheet, content: {
+                        getActionSheet()
+                    })
+                    
                 }
                 .padding(.all,6)
             }
@@ -79,8 +95,7 @@ struct PostView: View {
                         Image(systemName: "bubble.middle.bottom")
                             .font(.title3)
                             .foregroundColor(.primary)
-                        })
-                   
+                    })
                     
                     Image(systemName: "paperplane")
                         .font(.title3)
@@ -127,14 +142,60 @@ struct PostView: View {
         self.post = updatedPost
         
     }
-}
+    
+    func getActionSheet() -> ActionSheet {
+        
+        switch self.actionSheetType {
+        case .general:
+            return ActionSheet(title: Text("What would you like to do?"), message: nil, buttons: [
+                .destructive(Text("Report"), action: {
+                    
+                    self.actionSheetType = .reporting
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.5, execute: {
+                        self.showActionSheet.toggle()
+                    })
+                    
+                }),
 
-struct PostView_Previews: PreviewProvider {
+                .default(Text("Learn more..."), action: {
+                    print("LEARN MORE PRESSED")
+                }),
+                
+                .cancel()
+            ])
+        case.reporting:
+            return ActionSheet(title: Text("Why are you reporting this post?"), message: nil, buttons: [
+                .destructive(Text("This is inappropriate"), action: {
+                    reportPost(reason: "This is inappropriate")
+                }),
+                
+                .destructive(Text("This is spam"), action: {
+                    reportPost(reason: "This is spam")
+                }),
+                
+                .destructive(Text("It made me uncomfortable"), action: {
+                    reportPost(reason: "It made me uncomfortable")
+                }),
+                
+                .cancel({
+                    self.actionSheetType = .general
+                })
+            ])
+        }
+        
+    }
     
-    static var post: PostModel = PostModel(postID: "", userID: "", username: "Kuy", caption: "My first post!", dateCreated: Date(), likeCount: 0, likedByOwner: false)
+    func reportPost(reason: String) {
+        print("REPORT POST")
+    }
     
-    static var previews: some View {
-        PostView(post: post, showHeaderAndFooter: true, addHeartAnimationToView: true)
-            .previewLayout(.sizeThatFits)
+    struct PostView_Previews: PreviewProvider {
+        
+        static var post: PostModel = PostModel(postID: "", userID: "", username: "Kuy", caption: "My first post!", dateCreated: Date(), likeCount: 0, likedByOwner: false)
+        
+        static var previews: some View {
+            PostView(post: post, showHeaderAndFooter: true, addHeartAnimationToView: true)
+                .previewLayout(.sizeThatFits)
+        }
     }
 }
