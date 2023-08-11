624c9f48ec5caefd1b52399d7366a0369bb37590
“kurniawanj26” <jayadi.kurniawan@gmail.com>
2023-08-11 12:35:19 +0700

    created a function to logging in user into the app

--- Kura/Services/Firebase/AuthService.swift
+++ Kura/Services/Firebase/AuthService.swift
@@ -48,6 +48,30 @@ class AuthService {
         
     }
     
+    func logInUserToApp(userID: String, handler: @escaping (_ success: Bool) -> ()) {
+        
+        // get the user info
+        getUserInfo(forUserID: userID) { (returnedName, returnedBio) in
+                        
+            if let name = returnedName, let bio = returnedBio {
+                // success login
+                print("Success getting user info while logging in")
+                handler(true)
+                
+                // set the users info into the app
+                
+                
+            } else {
+                // error login
+                print("Error getting user info while logging in")
+                handler(false)
+            }
+                
+        }
+        
+        
+    }
+    
     func createNewUserInDatabase(name: String, email: String, providerID: String, provider: String, profileImage: UIImage, handler: @escaping (_ userID: String?) ->()) {
         
         // set up user document with the user collection
@@ -77,6 +101,29 @@ class AuthService {
                 // SUCCESS
                 handler(userID)
             }
+        }
+        
+    }
+    
+    // MARK: GET USER FUNCTIONS
+    // forUserID -> global
+    // userID -> local
+    func getUserInfo(forUserID userID: String, handler: @escaping(_ name: String?, _ bio: String?) -> ()) {
+        
+        REF_USERS.document(userID).getDocument { (documentSnapshot, error) in
+            
+            if let document = documentSnapshot,
+               let name = document.get(DatabaseUserField.displayName) as? String,
+               let bio = document.get(DatabaseUserField.bio) as? String {
+                print("Success getting user info")
+                handler(name, bio)
+                return
+            } else {
+                print("Error getting user info")
+                handler(nil, nil)
+                return
+            }
+            
         }
         
     }
